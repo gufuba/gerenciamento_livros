89858c22442200a0852bc63a0932825fd3121cf0
gustavo-fumagalli <g.fumagalli006@gmail.com>
2026-03-16 19:52:27 -0300

    model criado

---
+++ src/main/java/com/fumagalli/gerenciamento_livros/models/LivroModel.java
@@ -0,0 +1,50 @@
+package com.fumagalli.gerenciamento_livros.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "TBL_LIVRO")
+public class LivroModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+    private String autor;
+    private Integer anoPublicacao;
+
+    public LivroModel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public Integer getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(Integer anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+}
---
+++ src/test/java/com/fumagalli/gerenciamento_livros/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fumagalli.gerenciamento_livros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
